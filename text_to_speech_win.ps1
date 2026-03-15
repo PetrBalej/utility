1132431e430b3318a9caf2490d911d6d4d8aba15
Petr Balej <petr@balej.net>
2026-03-15 13:12:51 +0100

    Fix output file extension and add speech functionality

--- text_to_speech_win.ps1
+++ text_to_speech_win.ps1
@@ -1,6 +1,7 @@
 Add-Type -AssemblyName System.Speech
 $voice = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $voice.SelectVoice("Microsoft David Desktop")
-$voice.SetOutputToWaveFile("C:\OUTPUT_AUDIO.waw")
+$voice.SetOutputToWaveFile("C:\OUTPUT_AUDIO.wav")
 $text = Get-Content "C:\INPUT_TEXT.txt" -Raw
-$
+$voice.Speak($text)
+$voice.Dispose()
